f28fcd43eb7188a9f6269384e4e56803affabf79
Daniel <923daniel@naver.com>
2024-06-18 10:36:33 +0900

    feat: 필터 결과페이지 구현
    - Filter api 연동
    - UI/UX 구현
    - Kakao map api로 지도 구현

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import LoginSuccess from "./pages/LoginSuccess";
+import FilteredResults from "./pages/FilteredResults";
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/loginSuccess" element={<LoginSuccess />} />
+        <Route path="/filteredResults" element={<FilteredResults />} />
       </Routes>
     </Router>
   );
