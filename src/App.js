cb1565e6cc7b3cec70498708f84cb73ae5ca022b
최수연 <tndus5383@gmail.com>
2024-06-18 14:03:31 +0900

    feat: 호스트 페이지 구현

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import LoginSuccess from "./pages/LoginSuccess";
+import HostPage from "./pages/HostPage";
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/loginSuccess" element={<LoginSuccess />} />
+        <Route path="/hosting" element={<HostPage />} />
       </Routes>
     </Router>
   );
